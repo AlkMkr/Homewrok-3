9b43ec47e8f0f07aa5557455e2348e19fa0a13a5
Neitore <alekseymkaale@gmail.com>
2022-08-29 23:13:56 +0300

    Eleks.clear()

--- Lesson3/Lesson3_1.py
+++ Lesson3/Lesson3_1.py
@@ -1,4 +1,5 @@
 Eleks = ['Jonh Miller', 'Mary Sue', 'Tanaka Tarou', 'Vasya Pupkin', 'Samuel Rodriguez']
 Toshiba = ['Tanaka Tarou', 'Steven Armstrong', 'Vasya Pupkin', 'Ostap Bender']
 Toshiba.extend(Eleks)
+Eleks.clear()
 print(f'List of employees of Toshiba + Eleks: {Toshiba}')
